cf771d259c074d423bb086a37c79334465ecf963
Clement Calmels <clement.calmels@free.fr>
2020-10-26 21:57:49 +0100

    test: show OpenGL version in egl headless test

--- tests/egl.cpp
+++ tests/egl.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include "egl.hpp"
+#include "gl.hpp"
 
 int main(int argc, char *argv[])
 {
 	if (!egl::init())
 		return -1;
 
-	std::cerr << "EGL version: " << egl::version() << std::endl;
+	std::cerr << "EGL version   : " << egl::version() << std::endl;
+	std::cerr << "OpenGL verison: " << gl::version() << std::endl;
 
 	egl::fini();
 	return 0;
